de1d00b4754ef9c4108626fdeed9cfcc9b2c9c08
aslam <aslam@malsa.ch>
2020-10-17 23:02:01 +0200

    Add php 7.4 and delete old stuff (#1)
    Co-authored-by: Aslambek Idrisov <aslambek.idrisov@phbern.ch>

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -9,14 +9,6 @@ VHOST_ROOT=/data/web/releases/current/
 update-ca-certificates
 update-ca-certificates --fresh
 
-
-#
-# Configure MSMTP
-#
-SSMTP_SETTINGS_PATH=/etc/msmtprc
-/bin/sed -i "s@{{ SSMTP_MAILHUB }}@${SSMTP_MAILHUB}@" ${SSMTP_SETTINGS_PATH}
-
-
 #
 # user id / group id
 #
@@ -57,64 +49,15 @@ mkdir -p /data/log/phpfpm
 chown -R ${USER}:${GROUP} /data/log/phpfpm
 
 
-
-
-#
-# Wait for DB
-#
-if [ ! -z ${DB_PORT_3306_TCP_ADDR} ] ; then
-	until nc -z ${DB_PORT_3306_TCP_ADDR} ${DB_PORT_3306_TCP_PORT}; do
-		echo "$(date) - Waiting for db..."
-		sleep 1
-	done
-fi
-
-
-#
-# Import Dump
-#
-if [ ! -z ${IMPORT_MYSQL_DUMP} ] ; then
-	MYSQL_TABLE_COUNT=`mysql --host="${DB_PORT_3306_TCP_ADDR}" --port="${DB_PORT_3306_TCP_PORT}" --user="${DB_ENV_MYSQL_USER}" --password="${DB_ENV_MYSQL_PASSWORD}" --database="${DB_ENV_MYSQL_DATABASE}" --execute="SHOW TABLES;" --batch --skip-column-names | wc -l`
-	if [ "${MYSQL_TABLE_COUNT}" == "0" ] ; then
-		echo "Importing mysql dump ${IMPORT_MYSQL_DUMP}..."
-		mysql --host="${DB_PORT_3306_TCP_ADDR}" --port="${DB_PORT_3306_TCP_PORT}" --user="${DB_ENV_MYSQL_USER}" --password="${DB_ENV_MYSQL_PASSWORD}" --database="${DB_ENV_MYSQL_DATABASE}" < ${IMPORT_MYSQL_DUMP}
-		echo "done."
-	else
-		echo "There's already data in the database. Aborting."
-	fi
-fi
-
 #
 # CONFIGURE
 #
 case ${APPLICATION_TYPE} in
 
-	LARAVEL)
+	LARAVEL|SYMFONY)
 		DOCUMENT_ROOT=${VHOST_ROOT}public/
 		SETTINGS_PATH=${VHOST_ROOT}/.env
 	;;
-	YII2)
-		DOCUMENT_ROOT=${VHOST_ROOT}web/
-	;;
-	TYPO3_7|TYPO3_8)
-		DOCUMENT_ROOT=${VHOST_ROOT}web/
-		cd ${DOCUMENT_ROOT}../Configuration/
-		ln -snf ${TYPO3_CONTEXT} current
-		;;
-	FLOW|FLOW_3|FLOW_4|NEOS|NEOS_2)
-		DOCUMENT_ROOT=${VHOST_ROOT}Web/
-		SETTINGS_PATH=${VHOST_ROOT}Configuration/${FLOW_CONTEXT}/Settings.yaml
-
-		mkdir -p ${VHOST_ROOT}Configuration/ ${VHOST_ROOT}Data/ ${VHOST_ROOT}Web/
-		chown -R ${USER}:${GROUP} ${VHOST_ROOT}Configuration/ ${VHOST_ROOT}Data/ ${VHOST_ROOT}Web/
-		chmod -R u+rwx,g+rwx ${VHOST_ROOT}Configuration/ ${VHOST_ROOT}Data/ ${VHOST_ROOT}Web/
-
-		if [ ! -f ${SETTINGS_PATH}.docker ];
-		then
-			cp /opt/docker/Settings.yaml.docker ${SETTINGS_PATH}.docker
-			cp /opt/docker/.env.docker ${SETTINGS_PATH}.docker
-		fi
-		;;
 	HTML|PHP|PHPFOX)
 		DOCUMENT_ROOT=${VHOST_ROOT}
 		;;
@@ -137,9 +80,6 @@ do
 	/bin/sed -i "s@{{ DB_USER }}@${DB_ENV_MYSQL_USER}@" ${SETTINGS_PATH}
 	/bin/sed -i "s@{{ DB_PASSWORD }}@${DB_ENV_MYSQL_PASSWORD}@" ${SETTINGS_PATH}
 
-	/bin/sed -i "s@{{ ELASTICSEARCH_HOST }}@elasticsearch@" ${SETTINGS_PATH}
-	/bin/sed -i "s@{{ ELASTICSEARCH_PORT }}@${ELASTICSEARCH_PORT_9200_TCP_PORT}@" ${SETTINGS_PATH}
-
 	/bin/sed -i "s@{{ PRIMARY_DOMAIN }}@${PRIMARY_DOMAIN}@" ${SETTINGS_PATH}
 	/bin/sed -i "s@{{ PRIMARY_SCHEMA }}@${PRIMARY_SCHEMA}@" ${SETTINGS_PATH}
 	/bin/sed -i "s@{{ HOSTNAME }}@`hostname`@" ${SETTINGS_PATH}
@@ -148,12 +88,7 @@ do
 	/bin/sed -i "s@{{ ENCRYPTION_KEY }}@${ENCRYPTION_KEY}@" ${SETTINGS_PATH}
 	/bin/sed -i "s@{{ APP_KEY }}@${APP_KEY}@" ${SETTINGS_PATH}
 
-	/bin/sed -i "s@{{ PIWIK_URL }}@${PIWIK_URL}@" ${SETTINGS_PATH}
-	/bin/sed -i "s@{{ PIWIK_TOKEN }}@${PIWIK_TOKEN}@" ${SETTINGS_PATH}
-
-	/bin/sed -i "s@{{ TYPO3_CONTEXT }}@${TYPO3_CONTEXT}@" ${SETTINGS_PATH}
-	/bin/sed -i "s@{{ FLOW_CONTEXT }}@${FLOW_CONTEXT}@" ${SETTINGS_PATH}
-	/bin/sed -i "s@{{ LARAVEL_CONTEXT }}@${LARAVEL_CONTEXT}@" ${SETTINGS_PATH}
+	/bin/sed -i "s@{{ APPLICATION_CONTEXT }}@${APPLICATION_CONTEXT}@" ${SETTINGS_PATH}
 done
 
 
@@ -163,55 +98,16 @@ done
 
 case ${APPLICATION_TYPE} in
 	LARAVEL)
-		su ${USER} export -c "LARAVEL_CONTEXT=${LARAVEL_CONTEXT} ${VHOST_ROOT}php artisan cache:clear"
-		su ${USER} export -c "LARAVEL_CONTEXT=${LARAVEL_CONTEXT} ${VHOST_ROOT}php artisan route:clear"	
+		su ${USER} export -c "APPLICATION_CONTEXT=${APPLICATION_CONTEXT} ${VHOST_ROOT}php artisan cache:clear"
+		su ${USER} export -c "APPLICATION_CONTEXT=${APPLICATION_CONTEXT} ${VHOST_ROOT}php artisan route:clear"	
 	;;
-	YII2)
-	;;
-	TYPO3_7|TYPO3_8)
-		cd ${DOCUMENT_ROOT}
-		mkdir -p fileadmin typo3conf typo3temp/{GB,llxml,temp,_temp_,user_upload} typo3temp_processed_ uploads/{media,pics,tf}
-		chown -R ${USER}:${GROUP} fileadmin typo3conf typo3temp typo3temp_processed_ uploads
+	SYMFONY)
+	su ${USER} export -c "APPLICATION_CONTEXT=${APPLICATION_CONTEXT} ${VHOST_ROOT}php bin/console cache:clear"
 
-		# Clear cache
-		rm -f typo3cache/temp_CACHED_*
-		find typo3temp/ -type f -delete
-		mysql --host="${DB_PORT_3306_TCP_ADDR}" --port="${DB_PORT_3306_TCP_PORT}" --user="${DB_ENV_MYSQL_USER}" --password="${DB_ENV_MYSQL_PASSWORD}" --database="${DB_ENV_MYSQL_DATABASE}" --batch --execute="SHOW TABLES;" | grep -E "^(cache|cf)" | while read T; do
-			mysql --host="${DB_PORT_3306_TCP_ADDR}" --port="${DB_PORT_3306_TCP_PORT}" --user="${DB_ENV_MYSQL_USER}" --password="${DB_ENV_MYSQL_PASSWORD}" --database="${DB_ENV_MYSQL_DATABASE}" --batch --execute="TRUNCATE TABLE ${T};"
-		done
-	;;
-	FLOW|FLOW_2|FLOW_3|NEOS|NEOS_2)
-		cd ${VHOST_ROOT}
-		mkdir -p Data Web
-		chown -R ${USER}:${GROUP} Configuration Data Web
-		find Data/Temporary/ -type f -name Lock -delete
-		find Data/Temporary/ -type f -name Flow.lock -delete
-		rm Configuration/PackageStates.php
-		find Configuration -type f -name "IncludeCachedConfigurations.php" -delete
-		rm Web/_Resources/Persistent/*
-		rm Web/_Resources/Static/Packages/*
-
-		su ${USER} export -c "FLOW_CONTEXT=${FLOW_CONTEXT} ${VHOST_ROOT}flow flow:cache:flush --force"
-		su ${USER} export -c "FLOW_CONTEXT=${FLOW_CONTEXT} ${VHOST_ROOT}flow doctrine:migrate --force"
-		su ${USER} export -c "FLOW_CONTEXT=${FLOW_CONTEXT} ${VHOST_ROOT}flow cache:warmup --force"
-
-		su ${USER} export -c "FLOW_CONTEXT=${FLOW_CONTEXT} ${VHOST_ROOT}flow node:repair"
-
-		su ${USER} export -c "FLOW_CONTEXT=${FLOW_CONTEXT} ${VHOST_ROOT}flow nodeindex:cleanup --force"
-		su ${USER} export -c "FLOW_CONTEXT=${FLOW_CONTEXT} php -d memory_limit=1024M ${VHOST_ROOT}flow nodeindex:build --workspace live --force" || echo "Error while indexing" &
 	;;
 	HTML|PHP|PHPFOX)
 	;;
 esac
-
-
-#
-# NEOS 2.0 Specific commands
-#
-
-if [ ${APPLICATION_TYPE} = "NEOS_2" ]; then
-	su ${USER} export -c "FLOW_CONTEXT=${FLOW_CONTEXT} ${VHOST_ROOT}flow resource:publish"
-fi
 
 
 #############################
